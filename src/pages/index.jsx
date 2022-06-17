252c2ae1ebc616d5e5046aa75b8abac50fa86072
honoka <h19981029@gmail.com>
2022-06-17 19:34:15 +0900

    ⚗️ 試しにクリックイベントを書いてみた！

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,13 +3,21 @@ import styles from '../styles/Home.module.css'
 import { Footer } from 'src/components/Footer'
 import { Main } from 'src/components/Main'
 import { Header } from 'src/components/Header'
+import { useCallback } from 'react'
 
 export default function Home() {
+   const foo = 1;
+   const handleClick = useCallback((e) =>{
+    console.log(e.target.href);
+    e.preventDefault();
+    alert(foo);
+  },[]);
   return (
     <div className={styles.container}>
       <Head>
         <title>Index Page</title>
       </Head>
+      <a href='/about' onClick={handleClick}>ボタン</a>
       <Header/>
       <Main page="index" />
       <Footer />
