0fb713b9704b99a740a25ee6c4a121a2e6045ffa
honoka <h19981029@gmail.com>
2022-06-18 18:48:55 +0900

    ✨ useStateで真偽値の判定

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,32 +3,47 @@ import styles from '../styles/Home.module.css'
 import { Footer } from 'src/components/Footer'
 import { Main } from 'src/components/Main'
 import { Header } from 'src/components/Header'
-import { useCallback, useEffect , useState} from 'react'
+import { useCallback, useEffect, useState } from 'react'
 
 export default function Home() {
   const [count, setFoo] = useState(1);
+  const [text, setText] = useState("");
+  const [isShow, setIsShow] = useState(true);
 
   const handleClick = useCallback(() => {
-    if (count < 10){
-          setFoo(count => count + 1)
+    if (count < 10) {
+      setFoo(count => count + 1)
     }
-  },[count]);
+  }, [count]);
 
-  useEffect (() => {
+  const handleChange = useCallback(() => {
+    if (e.target.value.length > 5) {
+      alert("5文字以内 で入力してください");
+      return;
+    }
+  }, []);
+
+  const handleDisply = useCallback(() => {
+    setIsShow((isShow) => !isShow);
+  },[]);
+
+  useEffect(() => {
     document.body.style.backgroundColor = 'lightblue';
     return () => {
       document.body.style.backgroundColor = 'lightpink';
     }
-  },[count]);
+  }, [count]);
 
   return (
     <div className={styles.container}>
       <Head>
         <title>Index Page</title>
       </Head>
-      <h1>{count}</h1>
+      {isShow ? <h1>{count}</h1> : null}
       <button onClick={handleClick}>ボタン</button>
-      <Header/>
+      <button onClick={handleDisply}>非表示</button>
+      <input type="text" value={text} onChange={handleChange} />
+      <Header />
       <Main page="index" />
       <Footer />
     </div>
