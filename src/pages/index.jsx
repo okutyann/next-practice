86b1c6cb4d12690e4acb94f45f51c250d757e690
honoka <h19981029@gmail.com>
2022-06-18 16:57:32 +0900

    ✨ カウンターを設置した

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,21 +3,19 @@ import styles from '../styles/Home.module.css'
 import { Footer } from 'src/components/Footer'
 import { Main } from 'src/components/Main'
 import { Header } from 'src/components/Header'
-import { useCallback, useEffect } from 'react'
+import { useCallback, useEffect , useState} from 'react'
 
 export default function Home() {
-   const foo = 1;
-   const handleClick = useCallback((e) =>{
-    console.log(e.target.href);
-    e.preventDefault();
-    alert(foo);
-  },[]);
+  const [count, setFoo] = useState(1);
+  //let foo =1;
+
+  const handleClick = (e) =>{
+    setFoo(count => count + 1)
+  };
 
   useEffect (() => {
-    console.log('マウント時に実行される');
     document.body.style.backgroundColor = 'lightblue';
     return () => {
-      console.log('あんマウント時に実行される');
       document.body.style.backgroundColor = 'lightpink';
     }
   },[]);
@@ -27,7 +25,8 @@ export default function Home() {
       <Head>
         <title>Index Page</title>
       </Head>
-      <a href='/about' onClick={handleClick}>ボタン</a>
+      <h1>{count}</h1>
+      <button onClick={handleClick}>ボタン</button>
       <Header/>
       <Main page="index" />
       <Footer />
