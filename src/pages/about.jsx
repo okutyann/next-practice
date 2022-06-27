af2b965c3cea06d016788bf6a954bafc75e46386
honoka <h19981029@gmail.com>
2022-06-27 21:57:26 +0900

    🎨 カスタムフックスを使ってうまくまとめた

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -3,14 +3,35 @@ import styles from '../styles/Home.module.css'
 import { Footer } from '../components/Footer'
 import { Main } from '../components/Main'
 import { Header } from '../components/Header'
+import { useCounter } from 'src/hooks/useCounter'
+import { useInputArray } from 'src/hooks/useInputArray'
+import { useColor } from 'src/hooks/useColor'
 
-export default function Home() {
+
+export default function about() {
+  const {count , isShow , handleClick , handleDisply} = useCounter();
+  const {text , array , handleChange , handleAdd} = useInputArray();
+  useColor();
+
   return (
     <div className={styles.container}>
       <Head>
         <title>About Page</title>
       </Head>
       <Header />
+      {isShow ? <h1>{count}</h1> : null}
+      <button onClick={handleClick}>ボタン</button>
+      <button onClick={handleDisply}>非表示</button>
+
+      <input type="text" value={text} onChange={handleChange} />
+      <button onClick={handleAdd}>追加</button>
+      <ul>
+        {array.map(item => {
+          return (
+            <li key={item}>{item}</li>
+          )
+        })}
+      </ul>
       <Main page="about" />
       <Footer />
     </div>
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,48 +3,14 @@ import styles from '../styles/Home.module.css'
 import { Footer } from 'src/components/Footer'
 import { Main } from 'src/components/Main'
 import { Header } from 'src/components/Header'
-import { useCallback, useEffect, useState } from 'react'
+import { useCounter } from 'src/hooks/useCounter'
+import { useInputArray } from 'src/hooks/useInputArray'
+import { useColor } from 'src/hooks/useColor'
 
 export default function Home() {
-  const [count, setFoo] = useState(1);
-  const [text, setText] = useState("");
-  const [isShow, setIsShow] = useState(true);
-  const [array, setArray] = useState([]);
-
-  const handleClick = useCallback(() => {
-    if (count < 10) {
-      setFoo(count => count + 1)
-    }
-  }, [count]);
-
-  const handleChange = useCallback((e) => {
-    if (e.target.value.length > 5) {
-      alert("5文字以内 で入力してください");
-      return;
-    }
-    setText(e.target.value);
-  }, []);
-
-  const handleDisply = useCallback(() => {
-    setIsShow((prevIsShow) => !prevIsShow);
-  },[]);
-
-  const handleAdd = useCallback(() => {
-    setArray((prevArray) => {
-      if (prevArray.some(item => item === text)) {
-        alert("すでに登録されています");
-        return prevArray;
-      }
-      return [...prevArray, text];
-    });
-  },[text]);
-
-  useEffect(() => {
-    document.body.style.backgroundColor = 'lightblue';
-    return () => {
-      document.body.style.backgroundColor = 'lightpink';
-    }
-  }, [count]);
+  const {count , isShow , handleClick , handleDisply} = useCounter();
+  const {text , array , handleChange , handleAdd} = useInputArray();
+  useColor();
 
   return (
     <div className={styles.container}>
@@ -52,9 +18,11 @@ export default function Home() {
         <title>Index Page</title>
       </Head>
       <Header />
+
       {isShow ? <h1>{count}</h1> : null}
       <button onClick={handleClick}>ボタン</button>
       <button onClick={handleDisply}>非表示</button>
+
       <input type="text" value={text} onChange={handleChange} />
       <button onClick={handleAdd}>追加</button>
       <ul>
@@ -64,6 +32,7 @@ export default function Home() {
           )
         })}
       </ul>
+
       <Main page="index" />
       <Footer />
     </div>
