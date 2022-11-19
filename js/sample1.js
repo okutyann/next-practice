72f37b256d73b0bbd0f41fcfcb0a8f79408ab3a7
okutyann <h19981029@gmail.com>
2022-11-19 13:16:45 +0900

    ✨ constについて

---
+++ js/sample1.js
@@ -0,0 +1,10 @@
+const name1 = "hono";
+console.log(name1);
+
+const animal = {
+  name: "usagi",
+  age: 12,
+};
+console.log(animal);
+animal.name = "tori";
+console.log(animal);
---
+++ src/pages/index2.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Index2 = () => {
+  const honoka = "honoka";
+  console.log(honoka);
+
+  return (
+    <div>
+      <h1>2</h1>
+    </div>
+  );
+};
+
+export default Index2;
