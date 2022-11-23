b5986b2bc6ca74a16307eb5939490b2dc03bf418
okutyann <h19981029@gmail.com>
2022-11-23 09:07:09 +0900

    ✨ アロー関数

--- _practice/js/sample1.js
+++ _practice/js/sample1.js
@@ -1,10 +1,37 @@
-const name1 = "hono";
-console.log(name1);
+// const name1 = "hono";
+// console.log(name1);
 
-const animal = {
-  name: "usagi",
-  age: 12,
+// const animal = {
+//   name: "usagi",
+//   age: 12,
+// };
+// console.log(animal);
+// animal.name = "tori";
+// console.log(animal);
+
+// const name2 = "honoka";
+// const age = 24;
+// const message1 = "私の名前は" + name2 + "です。年齢は" + age + "です。";
+// const message2 = `私の名前は${name2}です。年齢は${age}です。`;
+
+function func(str) {
+  return str;
+}
+console.log(func("funcです。"));
+
+const func1 = function (str) {
+  return str;
 };
-console.log(animal);
-animal.name = "tori";
-console.log(animal);
+console.log(func1("func1です。"));
+
+const func2 = (str) => {
+  return str;
+};
+// returnの省略
+const func2 = (str) => str;
+console.log(func2("func2です。"));
+
+const func3 = (num1, num2) => {
+  return num1 + num2;
+};
+console.log(func3(10, 20));
