1865f066794627377cf69bcb5276a3b7ebef47e4
okutyann <h19981029@gmail.com>
2022-11-25 22:45:45 +0900

    分割代入 #1

--- _practice/js/sample1.js
+++ _practice/js/sample1.js
@@ -14,24 +14,50 @@
 // const message1 = "私の名前は" + name2 + "です。年齢は" + age + "です。";
 // const message2 = `私の名前は${name2}です。年齢は${age}です。`;
 
-function func(str) {
-  return str;
-}
-console.log(func("funcです。"));
+// function func(str) {
+//   return str;
+// }
+// console.log(func("funcです。"));
 
-const func1 = function (str) {
-  return str;
-};
-console.log(func1("func1です。"));
+// const func1 = function (str) {
+//   return str;
+// };
+// console.log(func1("func1です。"));
 
-const func2 = (str) => {
-  return str;
-};
-// returnの省略
-const func4 = (str) => str;
-console.log(func4("func4です。"));
+// const func2 = (str) => {
+//   return str;
+// };
+// // returnの省略
+// const func4 = (str) => str;
+// console.log(func4("func4です。"));
 
-const func3 = (num1, num2) => {
-  return num1 + num2;
-};
-console.log(func3(10, 20));
+// const func3 = (num1, num2) => {
+//   return num1 + num2;
+// };
+// console.log(func3(10, 20));
+
+// const myPofile = {
+//   name1: "honoka",
+//   age: 24,
+// };
+// const myPofile2 = {
+//   name1: "humi",
+//   age: 26,
+// };
+// const message1 = `私の名前は${myPofile.name1}です。年齢は${myPofile.age}歳です。`;
+// console.log(message1);
+
+// //これはできない
+// const { name1, age } = myPofile;
+// // const { name1, age } = myPofile2;
+
+// const message2 = `私の名前は${name1}です。年齢は${age}歳です。`;
+// console.log(message2);
+
+const myPofile = ["達文", 27];
+const message3 = `名前は${myPofile[0]}で年齢は${myPofile[1]}`;
+console.log(message3);
+
+const [name, age] = myPofile;
+const message4 = `名前は${name}です。${age}です`;
+console.log(message4);
