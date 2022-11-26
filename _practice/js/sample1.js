1781416fb695bf2ca39bd41b0578a5088d0fce39
okutyann <h19981029@gmail.com>
2022-11-26 11:09:59 +0900

    三項演算子 #5

--- _practice/js/sample1.js
+++ _practice/js/sample1.js
@@ -158,3 +158,24 @@
 //   }
 // });
 // console.log(newNameArry);
+
+/**
+ * 三項演算子（Reactでよく使う）
+ */
+//ある条件　？　条件がtrueの時　：　条件がfalseの時
+// const val1 = 1 > 0 ? "trueです。" : "falseです。";
+// console.log(val1);
+
+// //toLocaleString … カンマ区切りの数値に表示してくれる
+// const num = "1300";
+// //console.log(num.toLocaleString());
+
+// //typeof … 型が何かを判断してくれる
+// const formattedNum =
+//   typeof num === "number" ? num.toLocaleString() : "数値を入力してください。";
+// console.log(formattedNum);
+
+const checkSum = (num1, num2) => {
+  return num1 + num2 > 100 ? "１００を超えています。" : "許容範囲です。";
+};
+console.log(checkSum(30, 60));
