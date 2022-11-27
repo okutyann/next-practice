2489e997677339bfa54cd81996b958cf0e3d722f
okutyann <h19981029@gmail.com>
2022-11-27 21:26:35 +0900

    素のjavascriptを使ったTODOアプリの作成 #6 タスクの追加機能（ボタン）

--- _practice/todo/index.js
+++ _practice/todo/index.js
@@ -3,13 +3,32 @@
 const onClickAdd = () => {
   const inputText = document.getElementById("add-text").value;
   document.getElementById("add-text").value = "";
+
   const li = document.createElement("li");
   const div = document.createElement("div");
   div.className = "list-row";
+
   const p = document.createElement("p");
   p.innerText = inputText;
+
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
+  completeButton.addEventListener("click", () => {
+    alert("完了");
+  });
+
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+  deleteButton.addEventListener("click", () => {
+    alert("削除");
+  });
+
+  //タグの子要素に各要素を設定
   li.appendChild(div);
   div.appendChild(p);
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
+
   document.getElementById("imcomplete-list").appendChild(li);
   //console.log(li);
 };
