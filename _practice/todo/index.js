05b9d9e10b882b7129a4bcee42f05451e2a6c076
okutyann <h19981029@gmail.com>
2022-11-28 12:22:01 +0900

    素のjavascriptを使ったTODOアプリの作成 #6 タスクの完了機能

--- _practice/todo/index.js
+++ _practice/todo/index.js
@@ -13,17 +13,36 @@ const onClickAdd = () => {
 
   const completeButton = document.createElement("button");
   completeButton.innerText = "完了";
+
   completeButton.addEventListener("click", () => {
-    alert("完了");
+    //完了に要素を追加
+    const addTarget = completeButton.parentNode;
+    const text = addTarget.firstElementChild.innerHTML;
+
+    //押された削除ボタンの親タグを未完了リストから削除
+    deleteFromIncompleteList(completeButton.closest("li"));
+
+    // // //div以下を削除
+    addTarget.textContent = null;
+    const p = document.createElement("p");
+    p.innerText = text;
+    const backButton = document.createElement("button");
+    backButton.innerText = "戻す";
+    const li = document.createElement("li");
+
+    li.appendChild(addTarget);
+    addTarget.appendChild(p);
+    addTarget.appendChild(backButton);
+    console.log(li);
+
+    document.getElementById("complete-list").appendChild(li);
   });
 
   const deleteButton = document.createElement("button");
   deleteButton.innerText = "削除";
   deleteButton.addEventListener("click", () => {
     //押された削除ボタンの親タグを未完了リストから削除
-    const deleteTaret = deleteButton.closest("li");
-    document.getElementById("imcomplete-list").removeChild(deleteTaret);
-    alert("削除");
+    deleteFromIncompleteList(deleteButton.closest("li"));
   });
 
   //タグの子要素に各要素を設定
@@ -32,11 +51,13 @@ const onClickAdd = () => {
   div.appendChild(completeButton);
   div.appendChild(deleteButton);
 
-  document.getElementById("imcomplete-list").appendChild(li);
-  //console.log(li);
+  document.getElementById("incomplete-list").appendChild(li);
+};
+
+//未完了リストから指定の要素を削除
+const deleteFromIncompleteList = (target) => {
+  document.getElementById("incomplete-list").removeChild(target);
 };
 
 let addButton = document.getElementById("add-button");
-console.log(addButton);
-
 addButton.addEventListener("click", () => onClickAdd());
