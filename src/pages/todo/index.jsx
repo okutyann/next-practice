48ea55e133be2a18a68b1a43acdd6c902415fb8b
okutyann <h19981029@gmail.com>
2022-12-07 23:26:28 +0900

    Reactを使ったTODOアプリの作成 #7 タスクの戻す機能

--- src/pages/todo/index.jsx
+++ src/pages/todo/index.jsx
@@ -3,11 +3,8 @@ import styles from "../../styles/module/todo.module.css";
 
 export default function Index() {
   const [todoText, setTodoText] = useState("");
-  const [incompleteTodos, setIncompleteTodos] = useState([
-    "テスト１",
-    "テスト２",
-  ]);
-  const [completeTodos, setCompleteTodos] = useState(["テスト３"]);
+  const [incompleteTodos, setIncompleteTodos] = useState([]);
+  const [completeTodos, setCompleteTodos] = useState([]);
 
   const onChangeTodoText = (event) => {
     setTodoText(event.target.value);
@@ -31,6 +28,15 @@ export default function Index() {
     newIncompleteTodo.splice(index, 1);
 
     const newCompleteTodo = [...completeTodos, incompleteTodos[index]];
+    setIncompleteTodos(newIncompleteTodo);
+    setCompleteTodos(newCompleteTodo);
+  };
+
+  const onClickBack = (index) => {
+    const newCompleteTodo = [...completeTodos];
+    newCompleteTodo.splice(index, 1);
+
+    const newIncompleteTodo = [...incompleteTodos, completeTodos[index]];
     setIncompleteTodos(newIncompleteTodo);
     setCompleteTodos(newCompleteTodo);
   };
@@ -76,12 +82,17 @@ export default function Index() {
       <div className={styles.completeArea}>
         <p className="title">完了のtodo</p>
         <ul>
-          {completeTodos.map((todo) => {
+          {completeTodos.map((todo, index) => {
             return (
               <li key={todo}>
                 <div className={styles.listRow}>
                   <p>{todo}</p>
-                  <button className={styles.button}>戻す</button>
+                  <button
+                    className={styles.button}
+                    onClick={() => onClickBack(index)}
+                  >
+                    戻す
+                  </button>
                 </div>
               </li>
             );
