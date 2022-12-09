729c846d94c9da29c2f868a5e133088ae5a78640
okutyann <h19981029@gmail.com>
2022-12-09 23:35:42 +0900

    Reactを使ったTODOアプリの作成 #7 CSS-in-JS

--- src/components/todo/InputTodo.jsx
+++ src/components/todo/InputTodo.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 import styles from "../../styles/module/todo.module.css";
 
+const style = {
+  backgroundColor: "#c1ffff",
+  width: "400px",
+  height: "57px",
+  padding: "8px",
+  margin: "8px",
+  borderRadius: "8px",
+};
 export const InputTodo = (props) => {
   const { todoText, onChange, onClick } = props;
   return (
     <>
-      <div className={styles.inputArea}>
+      <div style={style}>
         <input placeholder="todoを入力" value={todoText} onChange={onChange} />
         <button className={styles.button} onClick={onClick}>
           追加
