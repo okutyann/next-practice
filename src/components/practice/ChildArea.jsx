25b5fc51d6f1e6c6b5efd3683cdf21e6f897ca3b
okutyann <h19981029@gmail.com>
2022-12-10 18:12:32 +0900

    再レンダリングの仕組みを知りレンダリングを最適化する #8 レンダリングコストの高いコンポーネント

--- src/components/practice/ChildArea.jsx
+++ src/components/practice/ChildArea.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 
 export const ChildArea = ({ open }) => {
+  const data = [...Array(2000).keys()];
+  data.forEach(() => {
+    console.log("ChildAreaがレンダリングされた");
+  });
   return (
     <>
       {open ? (
