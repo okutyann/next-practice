7cf384380a74ce6cc2dc937a4f394bb742af0ae4
okutyann <h19981029@gmail.com>
2023-02-11 13:40:57 +0900

    ♻️ フラグメントに変更

--- react_3/src/pages/_app.jsx
+++ react_3/src/pages/_app.jsx
@@ -3,11 +3,11 @@ import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
   return (
-    <div>
+    <>
       <Head>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Component {...pageProps} />
-    </div>
+    </>
   );
 }
