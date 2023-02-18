94c6364d3a2005e377d6aa0e4b76e91334177848
okutyann <h19981029@gmail.com>
2023-02-18 19:47:53 +0900

    ✨ useRouterを学びました

---
+++ react_3/src/hooks/useBgColor.jsx
@@ -0,0 +1,18 @@
+import { useRouter } from "next/router";
+import { useEffect, useMemo } from "react";
+
+export const useBgColor = () => {
+  const router = useRouter();
+  console.log(router);
+
+  const bgColor = useMemo(() => {
+    return router.pathname === "/" ? "lightblue" : "beige";
+  }, [router.pathname]);
+
+  useEffect(() => {
+    document.body.style.background = bgColor;
+    return () => {
+      document.body.style.background = "";
+    };
+  }, [bgColor]);
+};
