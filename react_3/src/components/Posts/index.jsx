bd9661aa2f75eef85ae5989da045bd08dbc2de0f
okutyann <h19981029@gmail.com>
2023-02-22 08:23:39 +0900

    🎨 Replace to swr

--- react_3/src/components/Posts/index.jsx
+++ react_3/src/components/Posts/index.jsx
@@ -1,71 +1,19 @@
-import { useCallback, useEffect, useState, useReducer } from "react";
-
-const initialState = {
-  date: [],
-  loading: true,
-  error: null,
-};
-
-const reducer = (state, action) => {
-  switch (action.type) {
-    case "end":
-      return {
-        ...state,
-        date: action.date,
-        loading: false,
-      };
-    case "error": {
-      // console.log(action);
-      return {
-        ...state,
-        loading: false,
-        error: action.error,
-      };
-    }
-    // 上記の2つのどれにも当てはまらなかった場合
-    default:
-      throw new Error("no such action type!");
-  }
-};
+import { usePosts } from "@/hooks/usePosts";
 
 export const Posts = () => {
-  // const [posts, setPosts] = useState([]);
-  // const [loading, setLoading] = useState(true);
-  // const [error, setError] = useState(null);
-  const [state, dispatch] = useReducer(reducer, initialState);
-
-  const getPosts = useCallback(async () => {
-    try {
-      const res = await fetch("https://jsonplaceholder.typicode.com/posts");
-      if (!res.ok) {
-        throw new Error("エラーが発生したため、データの取得を失敗しました");
-      }
-      const json = await res.json();
-      dispatch({ type: "end", date: json });
-    } catch (error) {
-      console.log(error);
-      dispatch({ type: "error", error });
-    }
-  }, []);
-
-  useEffect(() => {
-    getPosts();
-  }, [getPosts]);
-
-  if (state.loading) {
+  const { data, error, isLoading, isEmpty } = usePosts();
+  if (isLoading) {
     return <div>ローディング中</div>;
   }
-  if (state.error) {
-    return <div>{state.error.message}</div>;
+  if (error) {
+    return <div>{error.message}</div>;
   }
-  console.log(state);
-  if (state.date.length === 0) {
+  if (isEmpty) {
     return <div>データは空です</div>;
   }
-
   return (
     <ol>
-      {state.date.map((post) => {
+      {data.map((post) => {
         return <li key={post.id}>{post.title}</li>;
       })}
     </ol>
