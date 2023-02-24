253c5ef8c6899e80494cf3e93fa47c2f4ac43b54
okutyann <h19981029@gmail.com>
2023-02-25 08:23:03 +0900

    🔥 不必要なコードやファイルを削除

--- react_3/src/pages/_app.jsx
+++ react_3/src/pages/_app.jsx
@@ -1,19 +1,13 @@
-import { useBgColor } from "@/hooks/useBgColor";
-import { useCounter } from "@/hooks/useCounter";
-import { useInputArray } from "@/hooks/useInputArray";
 import "@/styles/globals.css";
 import Head from "next/head";
 
 const App = ({ Component, pageProps }) => {
-  const counter = useCounter();
-  const inputArray = useInputArray();
-  useBgColor();
   return (
     <>
       <Head>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Component {...pageProps} {...counter} {...inputArray} />
+      <Component {...pageProps} />
     </>
   );
 };
